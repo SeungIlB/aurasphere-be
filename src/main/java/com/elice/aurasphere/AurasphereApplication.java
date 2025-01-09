9c488fca1510c3b79523087014d1bfbc98df63f2
SeungIlB <seoung1515@naver.com>
2025-01-09 13:37:34 +0900

    [Initial]프로젝트 생성

---
+++ src/main/java/com/elice/aurasphere/AurasphereApplication.java
@@ -0,0 +1,13 @@
+package com.elice.aurasphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurasphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AurasphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elice/aurasphere/AurasphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.aurasphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurasphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
