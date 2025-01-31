da23c04f5c85d6457a44bf1f21335c086fd0cfd9
SeungIlB <seoung1515@naver.com>
2025-01-31 17:01:09 +0900

    [Refactor] 생성시간 추가

--- src/main/java/com/elice/aurasphere/notification/dto/NotificationDTO.java
+++ src/main/java/com/elice/aurasphere/notification/dto/NotificationDTO.java
@@ -4,17 +4,21 @@ import com.elice.aurasphere.notification.entity.Notification;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class NotificationDTO {
     private Long id;
     private String message;
+    private LocalDateTime createdAt;
     private boolean isRead;
 
     public NotificationDTO(Notification notification) {
         this.id = notification.getId();
         this.message = notification.generateMessage();
-        this.isRead = notification.isRead();
+        this.createdAt = notification.getCreatedAt();
+        this.isRead = notification.getIsRead();
     }
 }
 
