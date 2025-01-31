938a29ed3d9a8e5434953184e29a0f7d8651ac94
SeungIlB <seoung1515@naver.com>
2025-01-31 11:46:36 +0900

    [Add] websocket 관련 data 파일 추가

---
+++ src/main/java/com/elice/aurasphere/notification/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.elice.aurasphere.notification.repository;
+
+import com.elice.aurasphere.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
+}
