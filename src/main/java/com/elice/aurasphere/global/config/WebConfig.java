3fe5a05def17a04e25cf3748079e6e332a431547
SeungIlB <seoung1515@naver.com>
2025-01-31 17:00:41 +0900

    [Refactor] WebConfig 경로 수정

--- src/main/java/com/elice/aurasphere/global/config/WebConfig.java
+++ src/main/java/com/elice/aurasphere/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.elice.aurasphere.config;
+package com.elice.aurasphere.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
