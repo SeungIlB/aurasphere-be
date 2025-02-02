69d366e37566715bfe4186eefaa80f40535a81d0
seungooan <wa594@naver.com>
2025-02-02 17:18:58 +0900

    [Fix] CORS 설정 변경

--- src/main/java/com/elice/aurasphere/global/config/SecurityConfig.java
+++ src/main/java/com/elice/aurasphere/global/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(allowedOrigins)); // 프론트엔드 주소
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(false);
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
